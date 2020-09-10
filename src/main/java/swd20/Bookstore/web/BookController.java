399d09b5fa9110096087e76188b866a55aa3c852
Tuukka Mattila <tuukka.mattila@myy.haaga-helia.fi>
2020-09-10 16:29:31 +0300

    Ensimmäinen lisäys

---
+++ src/main/java/swd20/Bookstore/web/BookController.java
@@ -0,0 +1,16 @@
+package swd20.Bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+	
+	//http://localhost:8080/index
+	@RequestMapping("/index")
+	public String Bookstore() {
+		return ":)";
+	}
+	
+
+}
